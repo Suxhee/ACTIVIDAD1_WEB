64952333620e95a540980a6ffbbe37ecab54c131
Racheloviedo <00033125@uca.edu.sv>
2026-09-17 06:32:56 -0600

    feat: agregar funciones auxiliares para validacion de incidencias

--- controllers/incidenciasControllers.js
+++ controllers/incidenciasControllers.js
@@ -1,32 +1,6 @@
 const db = require('../data/incidencias');
 const { esTextoValido, prioridadValida, normalizarPrioridad } = require('../utils/helpers');
 
-// 7)traduce la prioridad a una clasificacion
-function clasificarIncidencia(req, res) {
-  const id = parseInt(req.params.id);
-  const incidencia = db.incidencias.find(inc => inc.id === id);
-
-  if (!incidencia) {
-    return res.status(404).json({ mensaje: 'Incidencia no encontrada' });
-  }
-
-  let clasificacion;
-  switch (incidencia.prioridad) { // el switch obligatorio
-    case 'Alta':
-      clasificacion = 'Crítica';
-      break;
-    case 'Media':
-      clasificacion = 'Importante';
-      break;
-    case 'Baja':
-      clasificacion = 'Normal';
-      break;
-    default:
-      clasificacion = 'Sin clasificar';
-  }
-
-  res.json({ id: incidencia.id, clasificacion });
-}
 
 module.exports = {
   crearIncidencia,
