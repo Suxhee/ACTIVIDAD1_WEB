64570cbc3d860a680ac6f1e764206b46ed6954b2
Suxhee <160176238+Suxhee@users.noreply.github.com>
2026-09-16 22:50:46 -0600

    feat: se normalizo prioridad con toLowewrCase y se agrego validaciones flexible de mayusculas

---
+++ controllers/incidenciasControllers.js
@@ -0,0 +1,14 @@
+const db = require('../data/incidencias');
+const { esTextoValido, prioridadValida, normalizarPrioridad } = require('../utils/helpers');
+
+
+
+module.exports = {
+  crearIncidencia,
+  listarIncidencias,
+  buscarPorId,
+  cambiarEstado,
+  eliminarIncidencia,
+  obtenerEstadisticas,
+  clasificarIncidencia
+};
